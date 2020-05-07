84d445928ff21d3b5f0c8108c4bfa42be9851b09
Ricardo Ramirez <ricardo.ramirezlara@gmail.com>
2020-05-06 21:24:09 -0600

    Apply code formatting

--- src/components/question-wrapper.js
+++ src/components/question-wrapper.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import Questions from '../questions';
+import React from "react"
+import Questions from "../questions"
 
 function mergeQuestions(jsonQuestions = {}) {
   return Object.keys(jsonQuestions).reduce((all, current) => {
-    return all.push(...jsonQuestions[current]) && all;
-  }, []);
+    return all.push(...jsonQuestions[current]) && all
+  }, [])
 }
 
 function prepareInterview(questions = [], limit = 20) {
-  const maxQuestions = limit > questions.length ? questions.length : limit;
-  return [...questions].sort(() => .5 - Math.random()).slice(0, maxQuestions);
+  const maxQuestions = limit > questions.length ? questions.length : limit
+  return [...questions].sort(() => 0.5 - Math.random()).slice(0, maxQuestions)
 }
 
 export default ({ tech, level, isInterview = false }) => {
-  const questionFile = Questions[tech.toLowerCase()] || {};
-  let questions = level !== 'all' ? questionFile[level] : mergeQuestions(questionFile);
+  const questionFile = Questions[tech.toLowerCase()] || {}
+  let questions =
+    level !== "all" ? questionFile[level] : mergeQuestions(questionFile)
 
   if (isInterview) {
-    questions = prepareInterview(questions);
+    questions = prepareInterview(questions)
   }
-  
+
   return (
     <div className="w-full border-2 border-solid p-6 mt-10 mb-10">
-      <n3 className="text-xl font-semibold text-header">Showing questions for <span className="uppercase">{tech}</span> designed for <span className="uppercase">{level}</span> devs</n3>
+      <n3 className="text-xl font-semibold text-header">
+        Showing questions for <span className="uppercase">{tech}</span> designed
+        for <span className="uppercase">{level}</span> devs
+      </n3>
       <div className="border-b-2 mt-3 mb-3"></div>
-      {(questions || []).map(({question, answer}, idx) => (
+      {(questions || []).map(({ question, answer }, idx) => (
         <div className="mt-3 mb-3">
-          <h6 className="text-lg text-black mb-3 font-questions">{`${idx+1}. ${question}`}</h6>
+          <h6 className="text-lg text-black mb-3 font-questions">{`${
+            idx + 1
+          }. ${question}`}</h6>
           <p className="text-sm text-gray-700 font-answer">{answer}</p>
         </div>
       ))}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,72 @@
 import React, { useState } from "react"
-import QuestionWrapper from '../components/question-wrapper';
-import Questions from '../questions';
+import QuestionWrapper from "../components/question-wrapper"
+import Questions from "../questions"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const currentTechnologies = Object.keys(Questions);
-const levels = ['junior', 'medium', 'senior', 'all'];
+const currentTechnologies = Object.keys(Questions)
+const levels = ["junior", "medium", "senior", "all"]
 
 const IndexPage = () => {
-  const [tech, setTech] = useState('node');
-  const [level, setLevel] = useState('all');
-  const [interv, setInterv] = useState(false);
+  const [tech, setTech] = useState("node")
+  const [level, setLevel] = useState("all")
+  const [interv, setInterv] = useState(false)
 
   return (
     <Layout>
-    <SEO title="Home" />
-    <h3 className="mt-8 text-xl">1. Select a technology from the list</h3>
-    <p className="text-xs text-gray-800 mb-8">Technology instead of language to keep the tool open to extensions</p>
-    <div className="w-full h-auto mb-6 flex items-baseline flex-row flex-wrap">
-      {currentTechnologies.map(lang => (
-        <div className={`badge bg-green-200 ${tech === lang ? 'bg-green-800 text-white' : 'text-green-800'}`} onClick={() => setTech(lang)}>{lang}</div>
-      ))}
-    </div>
-    <h3 className="mt-8 text-xl">2. Select the level of the questions</h3>
-    <p className="text-xs text-gray-800 mb-8">We use the 3 base levels: Junior, Mid and Senior levels. Additionally, you can select 'ALL' that will show questions for all levels. This is useful if you want to interview a developer and you're not sure about its current level</p>
-    <div className="w-full h-auto mb-6 flex items-baseline flex-row flex-wrap">
-      {levels.map(sLevel => (
-        <div className={`badge bg-red-200 ${level === sLevel ? 'bg-red-800 text-white' : 'text-red-800' }`} onClick={() => setLevel(sLevel)}>{sLevel}</div>
-      ))}
-    </div>
-    <h3 className=" mt-8 text-xl">3. Optionally, enable Interview Mode</h3>
-    <p className="text-xs text-gray-800 mb-8">Interview mode will take 10 random questions from the criteria you defined. For example, if you selected Node@Junior, the 10 questions will be for that level</p>
-    <div className="w-full h-auto mb-6 flex items-baseline flex-row flex-wrap">
-      <div className={`badge bg-blue-200 ${interv ? 'bg-blue-800 text-white' : 'text-blue-800' }`} onClick={() => setInterv(!interv)}>{`${interv}`}</div>
-    </div>
-    <QuestionWrapper tech={tech} level={level} isInterview={interv} />
-  </Layout>
+      <SEO title="Home" />
+      <h3 className="mt-8 text-xl">1. Select a technology from the list</h3>
+      <p className="text-xs text-gray-800 mb-8">
+        Technology instead of language to keep the tool open to extensions
+      </p>
+      <div className="w-full h-auto mb-6 flex items-baseline flex-row flex-wrap">
+        {currentTechnologies.map(lang => (
+          <div
+            className={`badge bg-green-200 ${
+              tech === lang ? "bg-green-800 text-white" : "text-green-800"
+            }`}
+            onClick={() => setTech(lang)}
+          >
+            {lang}
+          </div>
+        ))}
+      </div>
+      <h3 className="mt-8 text-xl">2. Select the level of the questions</h3>
+      <p className="text-xs text-gray-800 mb-8">
+        We use the 3 base levels: Junior, Mid and Senior levels. Additionally,
+        you can select 'ALL' that will show questions for all levels. This is
+        useful if you want to interview a developer and you're not sure about
+        its current level
+      </p>
+      <div className="w-full h-auto mb-6 flex items-baseline flex-row flex-wrap">
+        {levels.map(sLevel => (
+          <div
+            className={`badge bg-red-200 ${
+              level === sLevel ? "bg-red-800 text-white" : "text-red-800"
+            }`}
+            onClick={() => setLevel(sLevel)}
+          >
+            {sLevel}
+          </div>
+        ))}
+      </div>
+      <h3 className=" mt-8 text-xl">3. Optionally, enable Interview Mode</h3>
+      <p className="text-xs text-gray-800 mb-8">
+        Interview mode will take 10 random questions from the criteria you
+        defined. For example, if you selected Node@Junior, the 10 questions will
+        be for that level
+      </p>
+      <div className="w-full h-auto mb-6 flex items-baseline flex-row flex-wrap">
+        <div
+          className={`badge bg-blue-200 ${
+            interv ? "bg-blue-800 text-white" : "text-blue-800"
+          }`}
+          onClick={() => setInterv(!interv)}
+        >{`${interv}`}</div>
+      </div>
+      <QuestionWrapper tech={tech} level={level} isInterview={interv} />
+    </Layout>
   )
 }
 
